22aca2abffe448d223562996cae570840778ea4e
AndreeaGabrian <93679566+AndreeaGabrian@users.noreply.github.com>
2025-03-26 05:01:59 +0100

    splited the css in two css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {MediaSectionComponent} from './media-section/media-section.component';
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, MediaSectionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css', './new.css']
 })
 export class AppComponent {
   @ViewChild('videoRef1') videoElement!: ElementRef;
